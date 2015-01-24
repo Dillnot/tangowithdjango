f21db2fd1199ec2ba08a6fa428caa6280e32bbd8
Dylan <stevensondylan13@googlemail.com>
2015-01-24 18:21:28 +0000

    chapter 7 - complete

---
+++ tango_with_django_project/rango/migrations/0002_category_slug.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rango', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='category',
+            name='slug',
+            field=models.SlugField(default=datetime.datetime(2015, 1, 24, 16, 52, 37, 557243, tzinfo=utc), unique=True),
+            preserve_default=False,
+        ),
+    ]
